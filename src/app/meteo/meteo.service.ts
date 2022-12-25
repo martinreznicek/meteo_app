01649255f652e6a3359f2de39121dd27b1366c80
Martin Řezníček <martin.reznicek@digiteqautomotive.com>
2022-12-25 11:28:16 +0100

    Add relevant weather icon

--- src/app/meteo/meteo.service.ts
+++ src/app/meteo/meteo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { Weather } from './models/weather.model';
 import { Config } from 'src/config';
 
@@ -11,8 +11,8 @@ export class MeteoService {
   constructor(private http: HttpClient) { }
 
   public get(lat: number, long: number): Promise<Weather> {
-    const url_par = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${Config.WEATHER_API_KEY}`
-    return this.http.get<Weather>(url_par).toPromise();
+    const urlWithParams = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${Config.WEATHER_API_KEY}`;
+    return this.http.get<Weather>(urlWithParams).toPromise();
   }
 
 }
